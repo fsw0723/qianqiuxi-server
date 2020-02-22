7ed35dc937df38c6c04457799361c236cd3b55bb
fsw0723 <fsw0723@gmail.com>
2020-02-22 19:21:44 +0800

    Add 30s ping-pong to set alive.

--- src/ws.js
+++ src/ws.js
@@ -2,20 +2,42 @@ const { Server } = require('ws');
 
 const handlers = require('./handlers');
 
+function noop() {}
+ 
+function heartbeat() {
+  this.isAlive = true;
+}
+
 module.exports.createServer = function(server) {
 	const wss = new Server({ server });
 	wss.games = [];
 
 	wss.on('connection', (ws) => {		
+		ws.isAlive = true;
+  		ws.on('pong', heartbeat);
+
 		handlers.handleNewPlayer(wss, ws);
 		ws.on('message', function (message) {
-			console.log('received: %s', message)
+			console.log(`[${new Date()}]received: %s`, message)
 
 			message = JSON.parse(message);
 			handlers.handlePairing(wss, ws, message);
 		});
 
-		ws.on('close', () => console.log('Client disconnected'));
+		ws.on('close', () => console.log('Client disconnected', new Date()));
+	});
+
+	const interval = setInterval(function ping() {
+	  	wss.clients.forEach(function each(ws) {
+		    if (ws.isAlive === false) return ws.terminate();
+
+		    ws.isAlive = false;
+		    ws.ping(noop);
+	  	});
+	}, 30000);
+
+	wss.on('close', function close() {
+	  clearInterval(interval);
 	});
 
 }
