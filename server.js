c696abd5ba5c81b725e85651f38441958fad732a
fsw0723 <fsw0723@gmail.com>
2020-03-06 16:36:40 +0800

    Set cache for images

--- server.js
+++ server.js
@@ -12,13 +12,16 @@ let app = express();
 let options = {
   setHeaders: function (res, path, stat) {
     res.set('Access-Control-Allow-Origin', '*');
+    if (path.endsWith('.jpg')) {
+      res.set('Cache-Control', 'public, max-age=86400');
+    }
   }
 }
 app.use(express.static('public', options));
 
 app.use(cors({
-    methods:['GET'],  //指定接收的请求类型
-}))
+  methods:['GET'],  //指定接收的请求类型
+}));
 
 const server = app
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
